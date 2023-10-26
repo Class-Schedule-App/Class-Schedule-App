4eb4f137947d5a0166700af1a825acf19ac17d20
ajackwere <austinewere59@gmail.com>
2023-10-26 10:19:15 +0300

    added users and mentors

---
+++ server/apps/users.py
@@ -0,0 +1,25 @@
+from models import db, User
+from main import app, Resource, api, request
+
+
+class UserResource(Resource):
+    def get(self):
+        users = User.query.all()
+        user_list = [user.to_dict() for user in users]
+        return user_list
+    
+    def post(self):
+        data = request.get_json()
+        user = User(
+            username=data['email'],
+            email=data['email'],
+            phone_number=data['phone_number'],
+            user_type=data['user_type']
+        )
+        db.session.add(user)
+        db.session.commit()
+        return user.to_dict(), 201
+
+
+api.add_resource(UserResource, '/users/')
+
