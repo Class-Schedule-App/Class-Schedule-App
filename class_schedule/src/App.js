1463a02c034220c1892f70c66ddf79482358e3ee
ajackwere <austinewere59@gmail.com>
2023-10-31 16:01:45 +0300

    Updated dashboard

--- class_schedule/src/App.js
+++ class_schedule/src/App.js
@@ -1,10 +1,42 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import React, {useState} from 'react';
+import LoginPage from './Pages/Login';
+import Dashboard from './Pages/Dashboard';
+import ProfilePage from './Pages/ProfilePage';
 
 function App() {
+
+  const [user, setUser] = useState(null);
+
+  // a function to handle user login
+  const handleLogin = (user) => {
+    setUser(user);
+  }
+
+  // a function to handle user logout.
+
+  const handleLogout = () => {
+    setUser(null);
+  };
+
+
   return (
-    <div className="">
-      <h1>Class Schedule App</h1>
-    </div>
+    <Router>
+      <div>
+        {/*header will be displayed here */}
+        {/* handleLout and user will be passed as props to the header */}
+        {user ? (
+          <Switch>
+            <Route path='/dashboard' component={Dashboard} />
+            <Route path='/profile' Component={ProfilePage}/>
+          </Switch>
+        ) : (
+          <Route path='/login' render={() => <LoginPage onLogin={handleLogin} />} />
+        )}
+
+      </div>
+    </Router>
   );
 }
 
--- class_schedule/src/Pages/Dashboard.js
+++ class_schedule/src/Pages/Dashboard.js
@@ -1,1 +1,19 @@
 // Develop the main dashboard page where users can see the list of sessions, announcements, and navigate to their profile.
+import React from 'react'
+import SessionList from '../Components/SessionList';
+import AnnouncementList from '../Components/AnnouncementList';
+
+
+
+export default function Dashboard() {
+  return (
+    <div>
+        <h1>Dashboard</h1>
+        <h2>Sessions</h2>
+        <SessionList />
+
+        <h2>Announcements </h2>
+        <AnnouncementList />
+    </div>
+  );
+}
