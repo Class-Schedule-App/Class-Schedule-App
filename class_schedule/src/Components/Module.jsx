a250856d813f8e869b524c8bce408ef28bc12717
ajackwere <austinewere59@gmail.com>
2023-10-31 09:22:22 +0300

    added module pages and component

---
+++ class_schedule/src/Components/Module.jsx
@@ -0,0 +1,1 @@
+// Create a component for displaying module information. This component can include the module name, description, list of sessions within the module, and the option to invite students.
---
+++ class_schedule/src/Pages/ModuleListPage.js
@@ -0,0 +1,3 @@
+// This will be for students
+// Create a page that lists all the modules available to a student. Students should be able to see the modules they have been invited to.
+// Each module can be presented as a card or a list item with its name and description. Clicking on a module should navigate to the ModuleDetailPage.
