61a4c6d17ee838f4b5441459484a3cf934bf0c2e
ajackwere <austinewere59@gmail.com>
2023-10-31 09:22:22 +0300

    added module pages and component

---
+++ class_schedule/src/Pages/ModuleDetailPage.js
@@ -0,0 +1,4 @@
+// for students
+// Build a page that displays detailed information about a selected module.
+// On this page, students can see the list of sessions within the module, session details, invitation links, and descriptions.
+// Provide options for students to interact with the module, such as liking and commenting on sessions within the module.
