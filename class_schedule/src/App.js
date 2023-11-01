269894e39996b296f2b5dfc855304901aa1c405c
ajackwere <austinewere59@gmail.com>
2023-11-01 11:44:20 +0300

    Added profile navigation from the Dashboard

--- class_schedule/src/App.js
+++ class_schedule/src/App.js
@@ -1,25 +1,24 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import React, {useState} from 'react';
-import LoginPage from './Pages/Login';
-import Dashboard from './Pages/Dashboard';
-import ProfilePage from './Pages/ProfilePage';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import LoginPage from "./Pages/Login";
+import Dashboard from "./Pages/Dashboard";
+import ProfilePage from "./Pages/ProfilePage";
 
 function App() {
-
   const [user, setUser] = useState(null);
+  // const [userRole, setUserRole] = useState(null);
 
   // a function to handle user login
   const handleLogin = (user) => {
     setUser(user);
-  }
+  };
 
   // a function to handle user logout.
 
   const handleLogout = () => {
     setUser(null);
   };
-
 
   return (
     <Router>
@@ -28,13 +27,15 @@ function App() {
         {/* handleLout and user will be passed as props to the header */}
         {user ? (
           <Routes>
-            <Route path='/dashboard' component={Dashboard} />
-            <Route path='/profile' Component={ProfilePage}/>
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/profile" Component={ProfilePage} />
           </Routes>
         ) : (
-          <Route path='/login' render={() => <LoginPage onLogin={handleLogin} />} />
+          <Route
+            path="/login"
+            render={() => <LoginPage onLogin={handleLogin} />}
+          />
         )}
-
       </div>
     </Router>
   );
--- class_schedule/src/Pages/Dashboard.js
+++ class_schedule/src/Pages/Dashboard.js
@@ -2,13 +2,27 @@
 import React from 'react'
 import SessionList from '../Components/SessionList';
 import AnnouncementList from '../Components/AnnouncementList';
-
+import ModuleListPage from '../Pages/ModuleListPage';
+import ModuleManagementPage from '../Pages/ModuleManagementPage';
+//import getUserRole from '../Pages/Login';
+import IconButton from '@mui/material/IconButton';
+import AccountCircleIcon from '@mui/icons-material/AccountCircleRounded'
+import { Link } from 'react-router-dom';
 
 
 export default function Dashboard() {
+  const userRole = getUserRole();
+
   return (
     <div>
+        <div style={{ position: 'absolute', top: '10px', right: '10px' }} >
+          <IconButton component={Link} to="/profile" color="primary" >
+            Profile
+          </IconButton>
+        </div>
         <h1>Dashboard</h1>
+        {userRole === 'mentor' ? <ModuleManagementPage /> : <ModuleListPage />}
+       
         <h2>Sessions</h2>
         <SessionList />
 
