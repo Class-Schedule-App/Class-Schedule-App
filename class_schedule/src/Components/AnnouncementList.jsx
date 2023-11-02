c4e60ef5fa52641b8af97cc6a20280fd9e9e452c
Blaque <blaque.develop@gmail.com>
2023-11-02 08:03:09 +0300

    Added Announcements

--- class_schedule/src/Components/AnnouncementList.jsx
+++ class_schedule/src/Components/AnnouncementList.jsx
@@ -1,1 +1,35 @@
 // Create a component to list course announcements. Each announcement can include the title, content, and date.
+
+import React from 'react';
+import { Card, CardContent, Typography, Button } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Grid from '@mui/material/Grid';
+
+const AnnouncementList = ({ announcements, onDelete }) => {
+  return (
+    <div>
+      {/* <h2>Announcements</h2> */}
+      <Grid container spacing={2}>
+        {announcements.map((announcement) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={announcement.id}>
+            <Card>
+              <CardContent>
+                <Typography variant="h6">{announcement.title}</Typography>
+                <Typography variant="body1">{announcement.body}</Typography>
+                <Typography variant="caption">
+                  Date: {new Date(announcement.date).toLocaleDateString()}
+                  <br />
+                  Time: {new Date(announcement.date).toLocaleTimeString()}
+                </Typography>
+                <Button variant="outlined" startIcon={<DeleteIcon />} onClick={() => onDelete(announcement.id)}>Delete</Button>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
+};
+
+export default AnnouncementList;
+
