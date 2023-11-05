149067f980cbdd8bea98048ab7818f05f1e257a3
ajackwere <austinewere59@gmail.com>
2023-11-05 15:56:20 +0300

    Added SessionList styles

--- class_schedule/src/Components/SessionList.jsx
+++ class_schedule/src/Components/SessionList.jsx
@@ -1,5 +1,6 @@
 // Create a component to display a list of available sessions. Each session can be a card with details like date, time, title, and description.
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const SessionList = () => {
   // State to store the list of sessions
@@ -8,13 +9,13 @@ const SessionList = () => {
 
   useEffect(() => {
     // Define the URL of your API
-    const apiUrl = 'https://class-schedule-pp4h.onrender.com/sessions';
+    const apiUrl = "https://class-schedule-pp4h.onrender.com/sessions";
 
     // Fetch the list of sessions
     fetch(apiUrl)
       .then((response) => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         return response.json();
       })
@@ -23,29 +24,40 @@ const SessionList = () => {
         setLoading(false);
       })
       .catch((error) => {
-        console.error('Error fetching sessions:', error);
+        console.error("Error fetching sessions:", error);
         setLoading(false);
       });
   }, []);
 
   return (
-    <div>
-      <h2 className='text-sm font-light italic'>List of Class Sessions:</h2>
-      {loading ? (
-        <p>Loading sessions...</p>
-      ) : sessions.length === 0 ? (
-        <p>No sessions available.</p>
-      ) : (
-        <ul>
-          {sessions.map((session) => (
-            <li key={session.session_id}>
-              <strong>{session.name}</strong>
-              <br />
-              {session.announcements}
-            </li>
-          ))}
-        </ul>
-      )}
+    <div className="bg-blue-100 min-h-screen flex items-center justify-center mx-auto ">
+      <div className="bg-white p-8 rounded-lg">
+        <h2 className="text-sm font-light italic">List of Class Sessions:</h2>
+
+        {loading ? (
+          <p>Loading sessions...</p>
+        ) : sessions.length === 0 ? (
+          <p>No sessions available.</p>
+        ) : (
+          <ul>
+            {sessions.map((session) => (
+              <li key={session.session_id}>
+                <strong>{session.name}</strong>
+                <br />
+                {session.announcements}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <p className="text-sm font-semibold absolute top-0 right-0 m-4">
+        <Link
+          to="/"
+          className="text-red-400 hover:text-red-700 focus:text-red-800"
+        >
+          Go Back to Dashboard?
+        </Link>
+      </p>
     </div>
   );
 };
