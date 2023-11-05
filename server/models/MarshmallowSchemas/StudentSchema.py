f8fd365389c868a58299865814c6f370e93bbcf4
KilonzoJames <jay.kilonzo@gmail.com>
2023-11-05 12:41:25 +0300

    Updated the marshmallow schemas

--- server/models/MarshmallowSchemas/StudentSchema.py
+++ server/models/MarshmallowSchemas/StudentSchema.py
@@ -1,10 +1,10 @@
-from marshmallow import fields
+from marshmallow import fields, validate
 from ..Student import Student
 from ..Config import ma
 
 class StudentSchema(ma.SQLAlchemyAutoSchema):
     name = fields.String(required=True)
-    email = fields.String(required=True)
+    email = fields.String(required=True, validate=[validate.Email()])
     profile_img = fields.String(required=True)
     created_at = fields.DateTime()
 
--- server/models/MarshmallowSchemas/UserSchema.py
+++ server/models/MarshmallowSchemas/UserSchema.py
@@ -1,14 +1,22 @@
-from marshmallow import fields
+from marshmallow import fields, validate, validates_schema, ValidationError
 from ..User import User
 from ..Config import ma
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
     username = fields.String(required=True)
-    email = fields.String(required=True)
+    email = fields.String(required=True, validate=[validate.Email()])
     phone_number = fields.Integer(required=True)
+    password = fields.String(required=True)
     created_at = fields.DateTime(required=True)
     updated_at = fields.DateTime(required=True)
     user_type = fields.String(required=True)
 
+    @validates_schema
+    def validate_email(self, data, **kwargs):
+        email = data.get("email")
+
+        if User.query.filter_by(email=email).count():
+            raise ValidationError(f"Email {email} already exists.")
     class Meta:
         model = User
+        exclude = ["id"]
