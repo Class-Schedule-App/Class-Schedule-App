4dfba65039f73a5d017098be2b1be3918426b002
ajackwere <austinewere59@gmail.com>
2023-11-08 08:42:01 +0300

    Updated the Module List

--- class_schedule/src/Pages/ModuleListPage.js
+++ class_schedule/src/Pages/ModuleListPage.js
@@ -1,42 +1,78 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { Typography, List, ListItem } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { Typography } from "@mui/material";
 
 const ModuleListPage = () => {
   const [modules, setModules] = useState([]);
+  const [showInviteLink, setShowInviteLink] = useState({});
 
   useEffect(() => {
     // Fetch modules data for students
     const fetchModules = async () => {
       try {
-        const response = await axios.get('https://class-schedule-pp4h.onrender.com/modules');
+        const response = await axios.get(
+          "https://class-schedule-pp4h.onrender.com/modules"
+        );
         setModules(response.data);
       } catch (error) {
-        console.error('Error fetching modules:', error);
+        console.error("Error fetching modules:", error);
       }
     };
 
     fetchModules();
   }, []);
 
+  const handleToggleShowInviteLink = (moduleId) => {
+    setShowInviteLink((prev) => ({
+      ...prev,
+      [moduleId]: !prev[moduleId]
+    }));
+  };
+
   return (
-    <div className="module-list-page">
-      <Typography variant="h6" gutterBottom>
+    <div className="module-list-page flex flex-wrap">
+      <Typography
+        variant="h6"
+        style={{ fontWeight: "600", alignItems: "center", content: "center" }}
+      >
         Available Modules:
       </Typography>
-      <List>
-        {modules.map((module) => (
-          <ListItem key={module.id} disablePadding>
-            <Link to={`/modules/${module.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
-              <Typography variant="h6">{module.module_name}</Typography>
-            </Link>
-            <Typography>Date: {module.date}</Typography>
-            <Typography>Time: {module.time}</Typography>
-            <Typography>Invite Link: {module.invite_link}</Typography>
-          </ListItem>
-        ))}
-      </List>
+      {modules.map((module) => (
+        <div
+          key={module.id}
+          className="border p-4 m-2 flex-1 min-w-1/4 max-w-1/3"
+        >
+          <Link
+            to={`/modules/${module.id}`}
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <Typography variant="h6">{module.module_name}</Typography>
+          </Link>
+          <Typography>Date: {module.date}</Typography>
+          <Typography>Time: {module.time}</Typography>
+          {showInviteLink[module.id] ? (
+            <div>
+              <Typography>Invite Link: {module.invite_link}</Typography>
+              <button
+                className="bg-blue-500 text-white border-none px-2 py-1 rounded mt-2 cursor-pointer hover:bg-blue-700"
+                onClick={() => handleToggleShowInviteLink(module.id)}
+              >
+                Show Less
+              </button>
+            </div>
+          ) : (
+            <div>
+              <button
+                className="bg-blue-500 text-white border-none px-2 py-1 rounded mt-2 cursor-pointer hover:bg-blue-700"
+                onClick={() => handleToggleShowInviteLink(module.id)}
+              >
+                Show More
+              </button>
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 };
